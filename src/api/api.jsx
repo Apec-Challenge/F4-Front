ddb1401ab815be29a92fb4eeaa97a4c954270a9a
DogonYu <dorogon01@gmail.com>
2020-10-29 11:35:07 +0900

    update: api

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -11,13 +11,56 @@ export const register = ({ email, password1, password2, nickname }) =>
   API.post(`/rest-auth/registration`, { email, password1, password2, nickname });
 
 /* Fund API */
-export const readFund = () => API.get(`/funds`);
-export const createFund = () => API.post(`/funds`);
-export const removeFund = () => API.post(`/funds`);
-export const updateFund = () => API.post(`/funds`);
+export const createFund = ({ title, content, fundingPrice, endedAt, place, user }) =>
+  API.post(`/funds`, { title, content, fundingPrice, endedAt, place, user });
+export const readFund = id => API.get(`/funds`, id);
+export const readFundList = () => API.get(`/funds`);
+export const updateFund = ({ title, content, fundingPrice, endedAt, place, user }) =>
+  API.put(`/funds`, { title, content, fundingPrice, endedAt, place, user });
+export const deleteFund = id => API.delete(`/funds`, { id });
 
 /* Place API */
-export const readPlace = () => API.get(`/places`);
-export const createPlace = () => API.post(`/places`);
-export const removePlace = () => API.post(`/places`);
-export const updatePlace = () => API.post(`/places`);
+export const createPlace = ({
+  title,
+  img,
+  description,
+  location,
+  lng,
+  lat,
+  mask,
+  handSnitizer,
+  disposableGloves,
+}) =>
+  API.post(`/places`, {
+    title,
+    img,
+    description,
+    location,
+    lng,
+    lat,
+    mask,
+    handSnitizer,
+    disposableGloves,
+  });
+export const readPlace = id => API.get(`/places`, id);
+export const readPlaceList = () => API.get(`/places`);
+export const updatePlace = ({
+  title,
+  img,
+  description,
+  businessHours,
+  location,
+  lng,
+  lat,
+  types,
+  PPE,
+}) =>
+  API.put(`/places`, { title, img, description, businessHours, location, lng, lat, types, PPE });
+export const deletePlace = id => API.delete(`/places`, id);
+
+/* Review */
+export const createReview = () => API.post(`/review`);
+export const readReview = () => API.get(`/review`);
+export const readReviewList = () => API.get(`/review`);
+export const updateReview = () => API.put(`/review`);
+export const deleteReview = () => API.post(`/review`);
